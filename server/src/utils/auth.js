8e108b0d699d600ef30e8b940faca4d841e9f60c
Manu Pal <manupal8954@gmail.com>
2026-01-22 10:00:28 +0530

    feat(auth): add password hashing utilities

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,2 +1,19 @@
 import jwt from 'jsonwebtoken';
-export const generateToken = (id) => jwt.sign({id}, 'secret');
+import bcrypt from 'bcryptjs';
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (enteredPassword, storedPassword) => {
+  return await bcrypt.compare(enteredPassword, storedPassword);
+};
+
+export { generateToken, hashPassword, comparePassword };
