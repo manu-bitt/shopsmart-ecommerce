66c75af6d394074daabce28707e2b49c5d5c0195
Manu Pal <manupal8954@gmail.com>
2026-01-24 15:00:18 +0530

    feat(product): fetch products from database

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,1 +1,6 @@
-export const getProducts = (req,res) => res.json([]);
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+export const getProducts = async (req,res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+};
